27e304ca303675b8c0a72072017025b794270c21
Mikel Urtxegi Garcia <mikel.urtxegi@pkf-attest.es>
2026-02-27 13:20:46 +0100

    fix(api): remove duplicated decks prefix in dashboard routes

--- decks/urls.py
+++ decks/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("decks/", views.create_deck, name="create_deck"),
-    path("decks/<int:deck_id>/cards/", views.deck_cards, name="deck_cards"),
+    path("", views.create_deck, name="create_deck"),
+    path("<int:deck_id>/cards/", views.deck_cards, name="deck_cards"),
 ]
